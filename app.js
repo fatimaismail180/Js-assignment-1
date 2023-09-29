36c63aa5b5e0a69a2d1efb8f764087a5bf007810
fatimaismail180 <145142834+fatimaismail180@users.noreply.github.com>
2023-09-29 22:55:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+alert("welcome");
+
+alert( "Error!please enter a valid password");
+
+alert("welcome to Js Land...\n Happy Coding!");
+
+alert("welcome to Js Land...");
+
+alert("Happy Coding!");
+
+console.log ("Hello.... I can run Js through my web brower's console");
+
